49c83f7a49a202f047b226d943df53bcaccb5cd0
sergiyz57918 <sergiy@networkcredo.net>
2019-08-04 14:26:15 +0000

    RSpeck definitions

--- spec/requests/orders_spec.rb
+++ spec/requests/orders_spec.rb
@@ -1,9 +1,43 @@
 require 'rails_helper'
 
+
 RSpec.describe 'OrderssController', type: :request  do
 
 
     describe 'POST /orders' do
+        let(:valid_attributes) {{ email: 'john@dow.com', itemId: 1 } }
+        let (:item_return){{
+                id: 1, 
+                description: "Sexy Leya",
+                price: 99.99, 
+                stockQty:1
+        }}
+        
+        let (:customer_return){{
+                email: 'john@dow.com',
+                lastName: 'Dow', 
+                firstName: 'John',
+                lastOrder: 99.99, 
+                lastOrder2: 99.99, 
+                lastOrder3: 99.99,  
+                award: 9.99
+        }}
+        
+        context 'when the request is valid' do
+            before { post '/orders', params: valid_attributes }
+        
+            it 'Get user and create order' do
+                item = class_double ("Item")
+                customer = class_double("Customer")
+                allow(item).to recive(:show).and_return(item_return)
+                allow(customer).to recive(:show).and_return(customer_return)
+                
+                expect(response).to have_http_status(201)
+                puts JSON.parse(response.body) 
+                
+                
+            end
+        end
     
     end
 
